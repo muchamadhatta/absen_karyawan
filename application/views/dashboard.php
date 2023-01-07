3289594b65f0dd3e89701d1f13335a9045a793cc
muchamadhatta <muchamadhatta96@gmail.com>
2023-01-07 08:23:04 +0700

    menambahkan dasboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -3,9 +3,9 @@
     <h1 class="display-4 my-0">APLIKASI ABSENSI</h1>
     <p class="lead">Aplikasi absensi digunakan untuk mempermudah dalam melakukan absen harian</p>
     <hr class="my-4">
-    <p class="lead">Peraturan dan tertib dalam bekerja</p>
-    <p class="lead">-Masuk jam 7 pulang jam 4 jika lebih akan dihitung lembur</p>
-    <p class="lead">-Masuk jam 8 pulang jam 5 jika lebih akan dihitung lembur</p>
+    <p class="lead">Peraturan dan tata tertib dalam bekerja</p>
+    <p class="lead">-Masuk jam 07:00 - 07:30 pulang jam 04:00 jika lebih akan dihitung lembur</p>
+    <p class="lead">-Apabila telat masuk tidak akan di hitung lembur</p>
     <p class="lead">-Selesaikan semua pesanan jika tidak ada maka stok</p>
     <p class="lead">-Minimal produksi harian 12 (kondisi saat ini) dapat berubah sesuai kondisi harga barang</p>
     <p class="lead">-Menjaga kebersihan serta merawat tempat guna kenyamanan bersama</p>
